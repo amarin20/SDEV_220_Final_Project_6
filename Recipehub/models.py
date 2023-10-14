3b4c4cef1dbdc7be0fd186e8f04b1f3c89245934
Arat0 <99769877+Arat0@users.noreply.github.com>
2023-10-14 15:00:51 -0400

    updated_database-Crud

--- Recipehub/models.py
+++ Recipehub/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+class UnitChoices(models.TextChoices):
+    TEASPOON = 'tsp', 'Teaspoon'
+    TABLESPOON = 'tbsp', 'Tablespoon'
+    CUP = 'cup', 'Cup'
+    OUNCE = 'oz', 'Ounce'
+    POUND = 'lb', 'Pound'
+    FLUID_OUNCE = 'fl oz', 'Fluid Ounce'
+    PINT = 'pint', 'Pint'
+    QUART = 'quart', 'Quart'
+    GALLON = 'gallon', 'Gallon'
+
+
 class Category(models.Model):
     name = models.CharField(max_length=225)
     
@@ -7,34 +19,33 @@ class Ingredient(models.Model):
     name = models.CharField(max_length=225)
     cost = models.FloatField()
     
-class equipment(models.Model):
-    name = models.CharField(max_length=225)
-    
 class Recipe(models.Model):
     title = models.CharField(max_length=255)
     selling_price = models.FloatField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    instructions = models.TextField()
 
     def profit_calculator(self):
-
         cost_price = 0
-        recipe_indgredients = RecipeIngredient.object.filter(recipe=self)
+        recipe_ingredients = RecipeIngredient.objects.filter(recipe=self)
 
-        for recipe_ingredient in recipe_indgredients:
-            ingredient = recipe_indgredients.ingredient 
+        for recipe_ingredient in recipe_ingredients:
+            ingredient_cost = recipe_ingredient.ingredient.cost
             quantity = float(recipe_ingredient.quantity)
-            ingredient_cost = Ingredient.cost
             cost_price += quantity * ingredient_cost
 
         profit = self.selling_price - cost_price 
-        return profit 
+        return profit
     
 class RecipeIngredient(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    quantity = models.CharField(max_length=255)
+    quantity = models.FloatField()
+    unit = models.CharField(max_length=50, choices=UnitChoices.choices, default=UnitChoices.OUNCE)
 
-class RecipeEquipment(models.Model):
+class RecipeStep(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    equipment = models.ForeignKey(equipment, on_delete=models.CASCADE)
+    step_number = models.PositiveIntegerField()  # Step sequence
+    step_description = models.TextField()
+    
+
+
--- Recipehub/urls.py
+++ Recipehub/urls.py
@@ -3,4 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.recipe_path, name='recipe_path'),
+    path('upload_recipe/', views.upload_recipe_view, name='upload_recipe'),
+    path('recipes/', views.recipe_list, name='recipe_list'),
+    path('recipes/<int:pk>/', views.recipe_detail, name='recipe_detail'),
+    path('recipes/new/', views.recipe_create, name='recipe_create'),
+    path('recipes/<int:pk>/edit/', views.recipe_edit, name='recipe_edit'),
+    path('recipes/<int:pk>/delete/', views.recipe_delete, name='recipe_delete'),
+
+
+
 ]
